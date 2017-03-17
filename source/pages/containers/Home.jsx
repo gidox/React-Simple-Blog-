eb711d9fac9bb571fcd92f9790cbbf1c82480174
Arquimedes <guilandarquimedes@gmail.com>
2017-03-16 21:20:12 -0500

    resolved dependency conflic

--- source/pages/containers/Home.jsx
+++ source/pages/containers/Home.jsx
@@ -15,8 +15,6 @@ class Home extends Component {
       posts: [],
       loading: true,
     };
-
-    this.handleScroll = this.handleScroll.bind(this);
   }
 
   async componentDidMount() {
@@ -27,43 +25,7 @@ class Home extends Component {
       page: this.state.page + 1,
       loading: false,
     })
-
-    window.addEventListener('scroll', this.handleScroll);
   }
-
-  componentWillUnmount(){
-    window.removeEventListener('scroll', this.handleScroll);
-
-  }
-
-  handleScroll(event){
-    if(this.state.loading) return null;
-
-    const scrolled = window.scrollY;
-    const viewportHeight = window.innerHeight;
-    const fullHeight = document.body.clientHeight;
-
-    if(!(scrolled + viewportHeight + 300 >= fullHeight)){
-      return null;
-    }
-
-    this.setState({loading: true}, async () => {
-      try {
-        const posts = await api.posts.getList(this.state.page);
-        this.setState({
-          posts: this.state.posts.concat(posts),
-          page: this.state.page + 1,
-          loading: false,
-        });
-      } catch(error){
-        console.error(error);
-        this.setState({loading: false});
-      }
-    })
-
-
-  }
-
   render() {
     return (
       <section name="Home">
